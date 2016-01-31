5a291c0ed9a43606aac671074d37cf311c820f96
Zura Guerra <guerrazuramx@gmail.com>
2016-01-31 12:00:13 -0600

    Add json parser

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule Derviche.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:exfirebase, "~> 0.4.0"}]
+     {:exfirebase, "~> 0.4.0"},
+     {:exjsx, "~> 3.2"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
